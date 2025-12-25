723afb29a2c2e3997745cef4802eaf4749b71e87
MisterFredo <mister.fredo@gmail.com>
2025-12-25 23:57:03 +0100

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,7 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-8 text-center">
-      <h1 className="text-3xl font-semibold mb-4">Ratecard Frontend</h1>
-      <p className="text-gray-600 text-lg">
-        L’interface Ratecard est bien en ligne.<br />
-        Le module public sera bientôt disponible.
-      </p>
-    </main>
+    <div className="p-10 bg-red-100 text-blue-600">
+      Test CSS OK
+    </div>
   );
 }
