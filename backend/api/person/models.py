1e91b29c42c3d54dc91b5b88a4f169fd2ca0ab6d
MisterFredo <mister.fredo@gmail.com>
2025-12-25 16:52:49 +0100

    Create models.py

---
+++ backend/api/person/models.py
@@ -0,0 +1,21 @@
+# backend/api/person/models.py
+
+from pydantic import BaseModel
+from typing import Optional
+
+
+class PersonCreate(BaseModel):
+    id_company: str
+    name: str
+    title: Optional[str] = None
+    profile_picture_url: Optional[str] = None
+    linkedin_url: Optional[str] = None
+
+
+class PersonOut(BaseModel):
+    id_person: str
+    id_company: str
+    name: str
+    title: Optional[str]
+    profile_picture_url: Optional[str]
+    linkedin_url: Optional[str]
