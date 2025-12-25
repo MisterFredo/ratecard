636025972bfbc42d5dd8de9da2ff9dafd142381a
MisterFredo <mister.fredo@gmail.com>
2025-12-25 18:55:16 +0100

    Update __init__.py

--- backend/api/health/__init__.py
+++ backend/api/health/__init__.py
@@ -1,7 +1,5 @@
-# backend/api/health/__init__.py
-
 from fastapi import APIRouter
-from backend.api.health.routes import router as health_routes
+from .routes import router as health_routes
 
-router = APIRouter(tags=["Health"])
+router = APIRouter()
 router.include_router(health_routes)
