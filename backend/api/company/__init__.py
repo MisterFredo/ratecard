db91174964cae251f58fc1a763a02f05daca9222
MisterFredo <mister.fredo@gmail.com>
2025-12-25 19:14:37 +0100

    Update __init__.py

--- backend/api/company/__init__.py
+++ backend/api/company/__init__.py
@@ -1,5 +1,7 @@
+# backend/api/company/__init__.py
+
 from fastapi import APIRouter
 from api.company.routes import router as company_routes
 
-router = APIRouter(tags=["COMPANY"])
+router = APIRouter()
 router.include_router(company_routes)
