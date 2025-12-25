abe0d7508fd0195cb6c5789d787f6915e06f6ce3
MisterFredo <mister.fredo@gmail.com>
2025-12-25 19:13:13 +0100

    Update models.py

--- backend/api/articles/models.py
+++ backend/api/articles/models.py
@@ -4,13 +4,16 @@ from pydantic import BaseModel, Field
 from typing import Optional, List
 from datetime import datetime
 
+
 class AxeItem(BaseModel):
     type: str = Field(..., regex="^(TOPIC|COMPANY|PRODUCT)$")
     value: str
 
+
 class ArticlePersonItem(BaseModel):
     id_person: str
     role: Optional[str] = None
+
 
 class ArticleCreate(BaseModel):
     titre: str
@@ -25,6 +28,7 @@ class ArticleCreate(BaseModel):
     axes: List[AxeItem] = []
     companies: List[str] = []
     persons: List[ArticlePersonItem] = []
+
 
 class ArticleOut(BaseModel):
     id_article: str
