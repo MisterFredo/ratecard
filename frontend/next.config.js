de84dd76672993ddbb7245a9b50e96bddab4782d
MisterFredo <mister.fredo@gmail.com>
2025-12-26 18:29:54 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,17 +2,14 @@
 
 const nextConfig = {
   reactStrictMode: true,
-
-  // Oblige Next.js à exposer les fichiers statiques en mode standalone (Render)
   output: "standalone",
 
-  // Autorise vraiment /media/* à être servi en production
   async rewrites() {
     return [
       {
-        source: "/media/:path*",
-        destination: "/media/:path*", // ne réécrit pas, mais force Next à exposer le dossier
-      },
+        source: "/media/:folder/:file",
+        destination: "/api/media/raw/:folder/:file",
+      }
     ];
   },
 
@@ -23,7 +20,7 @@ const nextConfig = {
         hostname: "**",
       }
     ]
-  }
+  },
 };
 
 module.exports = nextConfig;
