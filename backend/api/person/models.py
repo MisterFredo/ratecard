a35cb1db844345985167eb06910e2379ed2e9034
MisterFredo <mister.fredo@gmail.com>
2025-12-27 13:02:17 +0100

    Update models.py

--- backend/api/person/models.py
+++ backend/api/person/models.py
@@ -1,21 +1,33 @@
-# backend/api/person/models.py
-
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 
 class PersonCreate(BaseModel):
-    id_company: str
+    id_company: Optional[str] = None  # Lien optionnel
     name: str
     title: Optional[str] = None
-    profile_picture_url: Optional[str] = None
+
+    media_picture_rectangle_id: Optional[str] = None
+    media_picture_square_id: Optional[str] = None
+
     linkedin_url: Optional[str] = None
+    description: Optional[str] = None
 
 
 class PersonOut(BaseModel):
     id_person: str
-    id_company: str
+    id_company: Optional[str]
     name: str
-    title: Optional[str] = None
-    profile_picture_url: Optional[str] = None
-    linkedin_url: Optional[str] = None
+    title: Optional[str]
+
+    media_picture_rectangle_id: Optional[str]
+    media_picture_square_id: Optional[str]
+
+    linkedin_url: Optional[str]
+    description: Optional[str]
+
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    is_active: Optional[bool] = True
+
