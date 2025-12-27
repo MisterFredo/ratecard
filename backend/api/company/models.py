9b84dda92e48ede496bd24af90d8df8fcd2da389
MisterFredo <mister.fredo@gmail.com>
2025-12-27 12:56:02 +0100

    Update models.py

--- backend/api/company/models.py
+++ backend/api/company/models.py
@@ -2,24 +2,28 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
-
 class CompanyCreate(BaseModel):
     name: str
-    logo_url: Optional[str] = None
-    logo_square_url: Optional[str] = None
-    linkedin_url: Optional[str] = None
-    website_url: Optional[str] = None    # ← OPTION FUTUR
     description: Optional[str] = None
+
+    media_logo_rectangle_id: Optional[str] = None
+    media_logo_square_id: Optional[str] = None
+
+    linkedin_url: Optional[str] = None
+    website_url: Optional[str] = None
 
 
 class CompanyOut(BaseModel):
     id_company: str
     name: str
-    logo_url: Optional[str]
-    logo_square_url: Optional[str]
-    linkedin_url: Optional[str]
-    website_url: Optional[str]            # ← OPTION FUTUR
     description: Optional[str]
 
-    created_at: Optional[datetime] = None  # ← RECOMMANDÉ
-    updated_at: Optional[datetime] = None  # ← RECOMMANDÉ
+    media_logo_rectangle_id: Optional[str]
+    media_logo_square_id: Optional[str]
+
+    linkedin_url: Optional[str]
+    website_url: Optional[str]
+
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    is_active: Optional[bool] = True
