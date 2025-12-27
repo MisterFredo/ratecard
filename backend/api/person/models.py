c38ce8dbd331dabcb5f36d842dbaf139f36e06bd
MisterFredo <mister.fredo@gmail.com>
2025-12-27 13:03:37 +0100

    Update models.py

--- backend/api/person/models.py
+++ backend/api/person/models.py
@@ -4,30 +4,30 @@ from datetime import datetime
 
 
 class PersonCreate(BaseModel):
-    id_company: Optional[str] = None  # Lien optionnel
+    id_company: Optional[str] = None
     name: str
     title: Optional[str] = None
+    description: Optional[str] = None
 
     media_picture_rectangle_id: Optional[str] = None
-    media_picture_square_id: Optional[str] = None
+    media_picture_square_id: Optional[str] = None  # carré = principale
 
     linkedin_url: Optional[str] = None
-    description: Optional[str] = None
 
 
 class PersonOut(BaseModel):
     id_person: str
     id_company: Optional[str]
+
     name: str
     title: Optional[str]
+    description: Optional[str]
 
     media_picture_rectangle_id: Optional[str]
     media_picture_square_id: Optional[str]
 
     linkedin_url: Optional[str]
-    description: Optional[str]
 
-    created_at: Optional[datetime] = None
-    updated_at: Optional[datetime] = None
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
     is_active: Optional[bool] = True
-
