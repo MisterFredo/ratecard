57f29146080ffb9b19a3c50b91f8686fc99f7c2f
MisterFredo <mister.fredo@gmail.com>
2026-01-01 16:09:41 +0100

    Update __init__.py

--- backend/api/visuals/__init__.py
+++ backend/api/visuals/__init__.py
@@ -7,7 +7,7 @@ from api.visuals.article import router as article_router
 
 router = APIRouter()
 
-router.include_router(company_router, prefix="/company", tags=["VISUALS"])
-router.include_router(person_router, prefix="/person", tags=["VISUALS"])
-router.include_router(axe_router, prefix="/axe", tags=["VISUALS"])
-router.include_router(article_router, prefix="/article", tags=["VISUALS"])
+router.include_router(company_router, prefix="/company")
+router.include_router(person_router, prefix="/person")
+router.include_router(axe_router, prefix="/axe")
+router.include_router(article_router, prefix="/article")
