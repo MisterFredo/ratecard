26dc1d05dac790e0e93a93d7ddf9466ebcf908a7
MisterFredo <mister.fredo@gmail.com>
2026-01-06 22:22:35 +0100

    Delete backend/api/lab_light directory

--- backend/api/lab_light/__init__.py
+++
@@ -1,7 +0,0 @@
-# backend/api/lab_light/__init__.py
-
-from fastapi import APIRouter
-from api.lab_light.routes import router as lab_routes
-
-router = APIRouter()
-router.include_router(lab_routes)
