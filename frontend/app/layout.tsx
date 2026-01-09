73e0386d3db4d68d9373210a189cef3f84839efc
MisterFredo <mister.fredo@gmail.com>
2026-01-09 20:29:58 +0100

    Create layout.tsx

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body className="bg-white text-gray-900 antialiased">
+        {children}
+      </body>
+    </html>
+  );
+}
