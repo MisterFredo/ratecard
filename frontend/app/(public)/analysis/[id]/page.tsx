3ea5642decc4ad9b7e036bfd70a0b9d043928956
MisterFredo <mister.fredo@gmail.com>
2026-01-09 19:52:35 +0100

    Create page.tsx

---
+++ frontend/app/(public)/analysis/[id]/page.tsx
@@ -0,0 +1,13 @@
+import AnalysisDrawer from "@/components/drawers/AnalysisDrawer";
+
+export default function AnalysisPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return (
+    <div className="max-w-4xl mx-auto py-12">
+      <AnalysisDrawer id={params.id} onClose={() => {}} />
+    </div>
+  );
+}
