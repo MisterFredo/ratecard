e086e7d93f520dfce1a8c7ee58fa4f97fa486de7
MisterFredo <mister.fredo@gmail.com>
2026-01-20 15:09:25 +0100

    Update page.tsx

--- frontend-curator/app/(public)/(.)analysis/[id]/page.tsx
+++ frontend-curator/app/(public)/(.)analysis/[id]/page.tsx
@@ -1,1 +1,11 @@
+"use client";
 
+import AnalysisDrawer from "@/components/drawers/AnalysisDrawer";
+
+export default function AnalysisInterceptPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return <AnalysisDrawer id={params.id} />;
+}
