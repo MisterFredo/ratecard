d596ea3fa5ef0a092b5048e3759aaca06aa052f1
MisterFredo <mister.fredo@gmail.com>
2026-01-20 15:46:00 +0100

    Create layout.tsx

---
+++ frontend-curator/app/(public)/layout.tsx
@@ -0,0 +1,17 @@
+import PublicClientLayout from "./PublicClientLayout";
+import DrawerHost from "@/components/drawers/DrawerHost";
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <PublicClientLayout>
+      {children}
+
+      {/* HOST CENTRAL DES DRAWERS (NEWS / ANALYSIS / MEMBER) */}
+      <DrawerHost />
+    </PublicClientLayout>
+  );
+}
