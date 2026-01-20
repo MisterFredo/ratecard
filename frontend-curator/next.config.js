a573ca54801d064349bdee50b23390c73e830c5c
MisterFredo <mister.fredo@gmail.com>
2026-01-20 15:48:29 +0100

    Create next.config.js

---
+++ frontend-curator/next.config.js
@@ -0,0 +1,25 @@
+/** @type {import("next").NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/media/:folder/:file",
+        destination: "/api/media/raw/:folder/:file",
+      },
+    ];
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
