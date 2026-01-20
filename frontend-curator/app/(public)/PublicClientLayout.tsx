026a8bfb0fd9671842f3bdb5b04f4c34ad8ce01d
MisterFredo <mister.fredo@gmail.com>
2026-01-20 15:44:43 +0100

    Create PublicClientLayout.tsx

---
+++ frontend-curator/app/(public)/PublicClientLayout.tsx
@@ -0,0 +1,17 @@
+import PublicTopNavShell from "./PublicShell";
+import { DrawerProvider } from "@/contexts/DrawerContext";
+
+export default async function PublicClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <DrawerProvider>
+      <PublicTopNavShell>
+        {children}
+      </PublicTopNavShell>
+      {/* DrawerHost est monté dans app/(public)/layout.tsx */}
+    </DrawerProvider>
+  );
+}
