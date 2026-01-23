839ecce8beaca0d57182d2ab19502e83bbe34651
MisterFredo <mister.fredo@gmail.com>
2026-01-23 01:41:07 +0100

    Update next.config.js

--- frontend-curator/next.config.js
+++ frontend-curator/next.config.js
@@ -3,14 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  // 🔑 OBLIGATOIRE pour importer du TS hors du projet
-  experimental: {
-    externalDir: true,
-  },
-
-  // 🔑 Transpilation du code partagé
-  transpilePackages: ["shared"],
-
   async rewrites() {
     return [
       {
