cdbb3948b624a1976f95ea58869a8b65d0d6d02f
MisterFredo <mister.fredo@gmail.com>
2026-01-23 09:22:30 +0100

    Update service.py

--- backend/core/content/service.py
+++ backend/core/content/service.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime, date
-from typing import Optional
+from typing import Optional, Dict, Any
 from google.cloud import bigquery
 
 from config import BQ_PROJECT, BQ_DATASET
