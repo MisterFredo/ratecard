80aee1bc4f0b65c32ca0159e4ab26581b35cc1c4
MisterFredo <mister.fredo@gmail.com>
2026-02-27 18:52:25 +0100

    Update buildEmailGmail.ts

--- frontend/components/newsletter/email/buildEmailGmail.ts
+++ frontend/components/newsletter/email/buildEmailGmail.ts
@@ -33,7 +33,7 @@ export function buildEmailGmail({
 
   const blocks = [
 
-    EmailHeaderGmail(headerConfig, introText),
+    EmailHeaderGmail(headerConfig),
 
     news.length > 0
       ? EmailNewsBlockGmail(news)
