c14ec18c4ce75b4ca841d7528ad4c978132017c6
MisterFredo <mister.fredo@gmail.com>
2026-03-03 00:02:14 +0100

    Update __init__.py

--- backend/api/solution/__init__.py
+++ backend/api/solution/__init__.py
@@ -1,1 +1,7 @@
+# backend/api/solution/__init__.py
 
+from fastapi import APIRouter
+from api.solution.routes import router as solution_routes
+
+router = APIRouter()
+router.include_router(solution_routes)
