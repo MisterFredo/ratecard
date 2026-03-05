b6f5dca4d85f96b12f5dbad872a65cee05b6de6d
MisterFredo <mister.fredo@gmail.com>
2026-03-06 00:49:15 +0100

    Update StepValidation.tsx

--- frontend/components/admin/content/steps/StepValidation.tsx
+++ frontend/components/admin/content/steps/StepValidation.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { api } from "@/lib/api";
 
-import MultiSelectTopics from "@/components/admin/MultiSelectTopics";
+import MultiSelectTopics from "@/components/admin/content/steps/MultiSelectTopics";
 import CompanySelector, { Company } from "@/components/admin/CompanySelector";
 import ConceptSelector, { Concept } from "@/components/admin/ConceptSelector";
 import SolutionSelector, { Solution } from "@/components/admin/SolutionSelector";
