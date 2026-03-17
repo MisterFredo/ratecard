3b2aa80016c2e4c27bbfefaee8dfe4f8c1d7312d
MisterFredo <mister.fredo@gmail.com>
2026-03-17 19:54:02 +0100

    Update models.py

--- backend/api/vector/models.py
+++ backend/api/vector/models.py
@@ -4,3 +4,13 @@ from typing import List
 
 class VectorBatchRequest(BaseModel):
     ids: List[str]
+
+class VectorSingleResponse(BaseModel):
+    status: str
+    news_id: str
+    nb_vectors: int
+
+
+class VectorBatchResponse(BaseModel):
+    status: str
+    results: List[dict]
