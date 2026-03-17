c3f89d6eb48a8a25739402bedeecb4954fd0e926
MisterFredo <mister.fredo@gmail.com>
2026-03-17 14:06:45 +0100

    Update RawDrawer.tsx

--- frontend/components/admin/stock/RawDrawer.tsx
+++ frontend/components/admin/stock/RawDrawer.tsx
@@ -59,7 +59,7 @@ export default function RawDrawer({
 
       await api.put(`/content/raw/update/${raw.id_raw}`, {
         source_title: title,
-        date_source: date ? `${date}T00:00:00` : null,
+        date_source: date || null,
         raw_text: rawText,
       });
 
