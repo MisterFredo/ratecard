fbabd017938604ed541605332cb142e8d26455b5
MisterFredo <mister.fredo@gmail.com>
2026-03-18 16:54:28 +0100

    Create FeedHeader.tsx

---
+++ frontend-curator/components/feed/FeedHeader.tsx
@@ -0,0 +1,12 @@
+export default function FeedHeader() {
+  return (
+    <div className="space-y-1">
+      <h1 className="text-3xl font-bold text-teal-700">
+        Feed
+      </h1>
+      <p className="text-gray-600 text-sm">
+        Suivez et explorez les signaux clés du marché
+      </p>
+    </div>
+  );
+}
