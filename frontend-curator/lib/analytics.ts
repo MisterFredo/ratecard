2e1ea5176827593e89491c7133b2bd3b2b91bff8
MisterFredo <mister.fredo@gmail.com>
2026-03-18 17:58:35 +0100

    Create analytics.ts

---
+++ frontend-curator/lib/analytics.ts
@@ -0,0 +1,11 @@
+export function trackEvent(
+  eventName: string,
+  params?: Record<string, any>
+) {
+  if (typeof window === "undefined") return;
+
+  const gtag = (window as any).gtag;
+  if (!gtag) return;
+
+  gtag("event", eventName, params || {});
+}
