3c72788372e02df94f360c20d8fe234e56af826c
MisterFredo <mister.fredo@gmail.com>
2026-03-19 14:23:49 +0100

    Update page.tsx

--- frontend-curator/app/(workspace)/topics/page.tsx
+++ frontend-curator/app/(workspace)/topics/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useDrawer } from "@/contexts/DrawerContext";
 import TopicCard from "@/components/topics/TopicCard";
 
 type TopicItem = {
