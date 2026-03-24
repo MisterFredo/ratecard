92c39e9a54e4eb2a391e835ed8b192d3d628aec0
MisterFredo <mister.fredo@gmail.com>
2026-03-24 21:25:25 +0100

    Create buildEmailGmail.tsx

---
+++ frontend-curator/components/newsletter/buildEmailGmail.tsx
@@ -0,0 +1,59 @@
+import type {
+  NewsletterNewsItem,
+  NewsletterAnalysisItem,
+  HeaderConfig,
+  TopicStat,
+} from "@/types/newsletter";
+
+import { EmailLayoutGmail } from "./EmailLayoutGmail";
+import { EmailHeaderGmail } from "./EmailHeaderGmail";
+import { EmailNewsBlockGmail } from "./EmailNewsBlockGmail";
+import { EmailStatsBlockGmail } from "./EmailStatsBlockGmail";
+import { EmailBrevesBlockGmail } from "./EmailBrevesBlockGmail";
+import { EmailAnalysesBlockGmail } from "./EmailAnalysesBlockGmail";
+import { EmailSignatureGmail } from "./EmailSignatureGmail";
+
+type Props = {
+  headerConfig: HeaderConfig;
+  introText?: string;
+  news: NewsletterNewsItem[];
+  breves: NewsletterNewsItem[];
+  analyses: NewsletterAnalysisItem[];
+  topicStats?: TopicStat[];
+};
+
+export function buildEmailGmail({
+  headerConfig,
+  introText,
+  news,
+  breves,
+  analyses,
+  topicStats = [],
+}: Props) {
+
+  const blocks = [
+
+    EmailHeaderGmail(headerConfig),
+
+    news.length > 0
+      ? EmailNewsBlockGmail(news)
+      : "",
+
+    headerConfig.showTopicStats && topicStats.length > 0
+      ? EmailStatsBlockGmail(topicStats)
+      : "",
+
+    breves.length > 0
+      ? EmailBrevesBlockGmail(breves)
+      : "",
+
+    analyses.length > 0
+      ? EmailAnalysesBlockGmail(analyses)
+      : "",
+
+    EmailSignatureGmail()
+
+  ].join("");
+
+  return EmailLayoutGmail(blocks);
+}
