413f6a2cc6287724a63c3a39f37e6f372618fd90
MisterFredo <mister.fredo@gmail.com>
2026-03-30 14:03:15 +0200

    Update __init__.py

--- backend/api/visuals/__init__.py
+++ backend/api/visuals/__init__.py
@@ -5,6 +5,7 @@ from api.visuals.person import router as person_router
 from api.visuals.topic import router as topic_router
 from api.visuals.event import router as event_router
 from api.visuals.news import router as news_router
+from api.visuals.source import router as source_router
 
 router = APIRouter()
 
@@ -13,3 +14,4 @@ router.include_router(person_router, prefix="/person")
 router.include_router(topic_router, prefix="/topic")
 router.include_router(event_router, prefix="/event")
 router.include_router(news_router, prefix="/news")
+router.include_router(news_router, prefix="/source")
