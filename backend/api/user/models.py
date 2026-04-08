5be653606520e69e96b7765965b41bc4e43600ca
MisterFredo <mister.fredo@gmail.com>
2026-04-08 16:49:47 +0200

    Create models.py

---
+++ backend/api/user/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class CreateUserPayload(BaseModel):
+    email: str
+    name: Optional[str] = None
+    company: Optional[str] = None
+    language: Optional[str] = "fr"
+
+
+class AssignUniversePayload(BaseModel):
+    user_id: str
+    universes: List[str]
