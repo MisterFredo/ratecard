bed6b3aca8cd43b67a2da9e23756e67655087186
MisterFredo <mister.fredo@gmail.com>
2026-04-10 19:31:53 +0200

    Update layout.tsx

--- frontend-curator/app/layout.tsx
+++ frontend-curator/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import CuratorShell from "@/components/layout/CuratorShell";
 import { DrawerProvider } from "@/contexts/DrawerContext";
 import DrawerHost from "@/components/drawers/DrawerHost";
+import { UniverseProvider } from "@/contexts/UniverseContext";
 
 export default function RootLayout({
   children,
@@ -12,12 +13,16 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body>
-        <DrawerProvider>
-          <CuratorShell>{children}</CuratorShell>
 
-          {/* 🔥 DRAWERS */}
-          <DrawerHost />
-        </DrawerProvider>
+        <UniverseProvider>
+          <DrawerProvider>
+            <CuratorShell>{children}</CuratorShell>
+
+            {/* 🔥 DRAWERS */}
+            <DrawerHost />
+          </DrawerProvider>
+        </UniverseProvider>
+
       </body>
     </html>
   );
