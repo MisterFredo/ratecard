a40690a4521399fa7eaab5fc67489a974a5b769a
MisterFredo <mister.fredo@gmail.com>
2026-04-17 17:03:36 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://ratecard-frontend.onrender.com",
+        "https://ratecard-frontend-prod.onrender.com",
+        "https://ratecard.fr",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
