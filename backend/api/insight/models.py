15499321640e7e6ca52e2b5d93bcec0d29d17c9c
MisterFredo <mister.fredo@gmail.com>
2026-05-09 18:20:12 +0200

    Update models.py

--- backend/api/insight/models.py
+++ backend/api/insight/models.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel
+from typing import List
 
+
+# ============================================================
+# REQUEST
+# ============================================================
+
+class InsightRequest(BaseModel):
+    ids: List[str]
+
+
+# ============================================================
+# RESPONSE
+# ============================================================
+
+class InsightResponse(BaseModel):
+    status: str
+    insight: str
